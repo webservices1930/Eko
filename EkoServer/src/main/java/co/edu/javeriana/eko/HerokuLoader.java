b2767c857e956145a943213e55b9f67a80b660c1
Lamasape <32495756+Lamasape@users.noreply.github.com>
2019-10-06 16:33:13 -0500

    Deploy0.1 Services Catalogo

--- EkoServer/src/main/java/co/edu/javeriana/eko/HerokuLoader.java
+++ EkoServer/src/main/java/co/edu/javeriana/eko/HerokuLoader.java
@@ -1,6 +1,8 @@
 package co.edu.javeriana.eko;
 
+import co.edu.javeriana.eko.services.CatalogoService;
 import co.edu.javeriana.eko.services.ProductoServices;
+import co.edu.javeriana.eko.services.UsuarioService;
 
 import javax.xml.ws.Endpoint;
 
@@ -13,5 +15,16 @@ public class HerokuLoader {
     String uri = "/eko/producto";
     ProductoServices service = new ProductoServices();
     Endpoint.publish(host + port + uri, service);
+
+    //Usuario
+    String uriUsuario ="/eko/usuario";
+    UsuarioService usuarioService = new UsuarioService();
+    Endpoint.publish(host + port + uriUsuario, usuarioService);
+
+    //Catalogo
+    String uriCatalogo ="/eko/catalogo";
+    CatalogoService catalogoUsuario = new CatalogoService();
+    Endpoint.publish(host + port + uriCatalogo, catalogoUsuario);
+
   }
 }
