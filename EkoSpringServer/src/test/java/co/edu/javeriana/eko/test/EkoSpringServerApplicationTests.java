2bd69dbeb6fa74fe63a1ead03fca90b5d7e491d2
Jesuspinarte <jesuspinarte@gmail.com>
2019-11-06 19:38:14 -0500

    Rename old files

--- EkoSpringServer/src/test/java/co/edu/javeriana/eko/test/EkoSpringServerApplicationTests.java
+++ EkoSpringServer/src/test/java/co/edu/javeriana/eko/test/EkoSpringServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jplanner;
+package co.edu.javeriana.eko.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JPlannerApplicationTests {
+public class EkoSpringServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
