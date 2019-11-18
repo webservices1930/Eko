04ed8274b8463309e72cc9cb0e3c8fcf6b26378c
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2019-11-17 20:19:38 -0500

    Pronostico

--- EkoClient/src/app/weather/weather.component.ts
+++ EkoClient/src/app/weather/weather.component.ts
@@ -8,6 +8,7 @@ import { WeatherService } from '../shared/weather/weather.service';
 })
 export class WeatherComponent implements OnInit {
   clima: any;
+  pronostico:any =[];
   temperatura = 0;
   @Input() ciudad:string='Bogotá';
   constructor(
@@ -19,11 +20,31 @@ export class WeatherComponent implements OnInit {
     .subscribe(response => {
       this.clima = response;
       this.temperatura = Math.round(this.clima.main.temp - 273.15);
-      console.log(this.clima);
     }, error =>{
       console.log('There was an error: ', error);
       console.log(error.status);
     });
+
+    this.weatherService.obtenerPronosticoClimaPorNombreCiudad(this.ciudad)
+    .subscribe(response => {
+      var aux = response;
+      let temp = '';
+      for(let a of aux.list){
+        if (temp != a.dt_txt.substring(0, 10)) {
+          temp = a.dt_txt.substring(0, 10);
+          console.log('Comparacion: '+temp+' - -'+a.dt_txt.substring(0, 10));
+          a.dt_txt = a.dt_txt.substring(0, 10);
+          a.main.temp = Math.round(a.main.temp - 273.15);
+          this.pronostico.push(a);
+        }
+      }
+      console.log(this.pronostico);
+
+    }, error =>{
+      console.log('There was an error: ', error);
+      console.log(error.status);
+    });
+
   }
 
 }
